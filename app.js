f3132af0753ef9d26038b3d5ac152ed2be8dbd0c
xiaoxiaocoder <xiaoxiaocoder@126.com>
2021-06-17 13:38:03 +0800

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path')
 
+var app = new express()
 app.use(express.static(path.resolve(__dirname, 'src/.vuepress/dist')))
 
 app.listen(80, function () {
